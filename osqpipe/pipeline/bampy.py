61ee8552868780dcf94f2f108db51dff441df0fc
Tim Rayner <tim.rayner@cruk.cam.ac.uk>
2015-02-23 14:38:58 +0000

    Added the bampy module (for easier manipulation of bam files via pysam) and a script to expand reaper/tally-generated bam file data to regular bam files.

---
+++ osqpipe/pipeline/bampy.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+#
+# $Id: bampy.py 3519 2015-02-23 12:07:21Z tfrayner $
+
+'''
+Code tools used to manipulate bam files from within python. This is
+intended to be a pure-python implementation for improved
+maintainability.
+'''
+
+import os
+import re
+import pysam
+from contextlib import contextmanager
+from logging import INFO
+from setup_logs import configure_logging
+LOGGER = configure_logging('bampy', level=INFO)
+
+class Bamfile(pysam.Samfile):
+
+  '''
+  Subclass of pysam.Samfile with a few convenience methods of our own.
+  '''
+
+  # Note that while it might be nice to have a Bamfile class which
+  # correctly auto-generates its own index, this is not going to be
+  # easily maintainable due to the implementation of
+  # pysam.Samfile. Rather than coding ourselves into knots, we have
+  # opted for a context manager approach (see open_bamfile).
+
+  def read_refname(self, read):
+    '''
+    Return the name of the reference sequence to which a given read
+    has been aligned.
+    '''
+    return self.getrname(read.tid)
+
+  def basename(self):
+    '''
+    Return the name of the underlying filename, minus its extension.
+    '''
+    return os.path.splitext(self.filename)[0]
+
+  def has_index_file(self):
+    '''
+    Simply establish whether a .bai index file is present for this bam
+    file.
+    '''
+    return os.path.exists("%s.bai" % self.filename)
+
+  def split_by_chromosome(self, pattern=None):
+    '''
+    Split reads by chromosome, writing out new bam files to disk and
+    returning new Bamfile objects. Takes an optional pattern argument
+    to enable filtering, e.g. for those genomes with many small
+    scaffolds.
+    '''
+    bamlist = []
+    if pattern is not None:
+        pattern = re.compile(pattern)
+    for refname in self.references:
+      if pattern is not None:
+        if not pattern.match(refname):
+          continue
+      LOGGER.info("Writing out reads for reference %s...", str(refname))
+      self.reset()
+      outfile = "%s_%s.bam" % (self.basename(), str(refname))
+      out = pysam.Samfile(outfile, 'wb', header=self.header)
+      for read in self.fetch(reference=refname):
+        out.write(read)
+      out.reset()
+      bamlist.append(Bamfile(filename=outfile))
+      
+    return bamlist
+
+  def expand_tallied_reads(self, outfile, keep_multi=False):
+    '''
+    Expand a bam file containing reaper/tally output, using the format
+    from the Odom Lab sequencing pipeline, into a bam file in which
+    all reads are represented by their own individual records. If the
+    keep_multi flag is set as True, multi-mapping reads will be kept.
+    Note that this can be misleading as multi-mapping reads are no
+    longer distributed randomly between mapped loci.
+    '''
+    LOGGER.info("Expanding tallied bam file %s", self.filename)
+    self.reset()
+    out = pysam.Samfile(outfile, 'wb', header=self.header)
+    for read in self.fetch(until_eof=True): # All reads in file, in order.
+
+      # If mapping_quality is zero, we assume this means it's
+      # multi-mapping (this is the bwa default).
+      if not keep_multi and read.mapping_quality == 0:
+        continue
+      readparts = read.query_name.split(':')
+      if len(readparts) == 2:
+        tally = readparts[1].split('_')
+        if len(tally) == 2 and tally[0] == 'count':
+          count = int(tally[1])
+        else:
+          raise StandardError(\
+            "Found read ID not matching expected tally output format: %s" % read.query_name)
+      else:
+        raise StandardError(\
+          "Found read ID not matching expected tally output format: %s" % read.query_name)
+      base_name = readparts[0] + ':read_'
+      for n in range(count):
+        read.query_name = base_name + str(n+1)
+        out.write(read)
+
+    out.close()
+    return Bamfile(filename=outfile)
+
+@contextmanager
+def open_bamfile(filename, index=True, *args, **kwargs):
+  '''
+  Context manager function which automatically creates .bai index
+  files if they're missing.
+  '''
+  if not os.path.exists("%s.bai" % filename) and index:
+    LOGGER.info("Indexing bam file %s...", filename)
+    pysam.index(filename)
+  bam = Bamfile(filename=filename, *args, **kwargs)
+  try:
+    yield bam
+  finally:
+    bam.close()
+
+if __name__ == '__main__':
+
+  # This is purely here as test code; do not use this as a production script!
+  import sys
+  with open_bamfile(filename=sys.argv[1]) as bam:
+    bam.split_by_chromosome()
+  
---
+++ util/expandTalliedBam.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+'''
+A script which can be used to expand an Odom Lab pipeline generated
+bam file, in which identical small RNA reads are compressed into a
+single record, into a more conventional bam file format (one read per
+record).
+'''
+
+from osqpipe.pipeline.bampy import open_bamfile
+
+################################################################################
+
+if __name__ == '__main__':
+
+  from argparse import ArgumentParser
+
+  P = ArgumentParser(description='Convert reaper/tally output bam files into'
+                     + ' conventional bam format (one read per record).')
+
+  P.add_argument('-t', '--tallied', dest='input', type=str, required=True,
+                 help='The reaper/tally-generated bam file to expand.')
+
+  P.add_argument('-e', '--expanded', dest='output', type=str, required=True,
+                 help='The name of the output expanded bam file.')
+
+  ARGS = P.parse_args()
+
+  with open_bamfile(ARGS.input) as bam:
+    bam.expand_tallied_reads(ARGS.output)
+    
