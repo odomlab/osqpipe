1830587a4d1500f3e135a96e58288f10b871825e
Tim Rayner <tim.rayner@cruk.cam.ac.uk>
2015-07-23 15:21:24 +0100

    Minor code formatting fixes for readability. Also fixed a potential bug where user=None in transfer_via_scp

--- bin/moveFileToArchive.py
+++ bin/moveFileToArchive.py
@@ -24,13 +24,20 @@ from logging import INFO, WARNING
 LOGGER = configure_logging(level=WARNING)
 CONFIG = Config()
 
-def transfer_over_scp(f1, f2, port=22, user=None, attempts = 1, sleeptime = 2):
-  '''A wrapper for scp allowing multipe attempts for the transfer in case of recoverable error.'''
+################################################################################
+def transfer_over_scp(f1, f2, port=22, user=None,
+                      attempts = 1, sleeptime = 2):
+  '''
+  A wrapper for scp allowing multipe attempts for the transfer in case
+  of recoverable error.
+  '''
   attempt = 0
-  known_exit_messages = ['No such file or directory','Failed to add the host to the list of known hosts','Operation not permitted']
+  known_exit_messages = ['No such file or directory',
+                         'Failed to add the host to the list of known hosts',
+                         'Operation not permitted']
 
   cmd = ''
-  if user is None:
+  if user is not None:
     cmd = 'scp -p -o StrictHostKeyChecking=no -P %s %s %s@%s' % (port, f1, user, f2)
   else:
     cmd = 'scp -p -o StrictHostKeyChecking=no -P %s %s %s' % (port, f1, f2)
@@ -66,8 +73,9 @@ def transfer_over_scp(f1, f2, port=22, user=None, attempts = 1, sleeptime = 2):
   return retcode
 
 def create_foreign_dir(host, port, user, folder):
-  '''Create folder in foreign host over ssh.'''
-
+  '''
+  Create folder in foreign host over ssh.
+  '''
   cmd = 'ssh -p %s %s@%s \'mkdir %s\'' % (port, user, host, folder)
   
   p = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
@@ -84,8 +92,11 @@ def create_foreign_dir(host, port, user, folder):
   
 
 def get_files_for_filetype(filetype, not_archived=False):
-  '''Returns file names of all files for a given file type. Currently, only two file types, fq and bam, are supported. In case of not_archived, returns files that have not been archived.'''
-
+  '''
+  Returns file names of all files for a given file type. Currently,
+  only two file types, fq and bam, are supported. In case of
+  not_archived, returns files that have not been archived.
+  '''
   files = []
 
   if filetype == 'fq':
@@ -127,13 +138,18 @@ def move_file_to_archive(fpath, archive, force_overwrite=False, force_delete=Fal
   Given a file name (or file path), and the name of an archive as recorded
   in the repository, make sure there is a valid copy of the file in the
   archive and delete the file from the primary repository file tree.
-  Force_overwrite forces file in archive as well as archiving record in the database to be overwritten.
-  Force_delete forces file in the source to be deleted even less than nr of days (specified in archive_location table) have passed since archiving.
-  Force_md5_check forces md5 sum of the archived file to be checked against the record in the repository.
-  Copy_only prevents the archived file to be registered as archived. This is handy in case we want to move the files to archive first and register them
-  as archived some other time.
-  '''
 
+  Arguments:
+  force_overwrite: forces file in archive as well as archiving record in the
+                   database to be overwritten.
+  force_delete: forces file in the source to be deleted even less than nr of days
+                (specified in archive_location table) have passed since archiving.
+  force_md5_check: forces md5 sum of the archived file to be checked against the
+                   record in the repository.
+  copy_only: prevents the archived file to be registered as archived. This is handy
+             in case we want to move the files to archive first and register them
+             as archived some other time.
+  '''
   archloc = ArchiveLocation.objects.get(name=archive)
 
   fname = os.path.basename(fpath)
@@ -141,7 +157,9 @@ def move_file_to_archive(fpath, archive, force_overwrite=False, force_delete=Fal
   if parts[1] == CONFIG.gzsuffix:
     fname = parts[0]
   fobj = find_file(fname)
-  # Setting archive=None allows retrieval of repository location of the file. NB! Do not save this as it would modify the object in db!
+  # Setting archive=None allows retrieval of repository location of
+  # the file. NB! Do not save this as it would modify the object in
+  # db!
   archivetmp = fobj.archive
   fobj.archive = None
   repopath = fobj.repository_file_path
@@ -155,34 +173,42 @@ def move_file_to_archive(fpath, archive, force_overwrite=False, force_delete=Fal
     if force_overwrite:
       fobj.archive = archloc
       fobj.archive_date = time.strftime('%Y-%m-%d')
-      # fobj.save() # Do we actually need to save it here, or can we make the transaction scope smaller FIXME?
+      # fobj.save() # Do we actually need to save it here, or can we
+      # make the transaction scope smaller FIXME?
       LOGGER.warning("Force overwrite. Updating archive record for %s." % fname)
   else:
     fobj.archive = archloc
     if not copy_only:
       fobj.archive_date = time.strftime('%Y-%m-%d') # NB! date format not tested!
-      # fobj.save() # Do we actually need to save it here, or can we make the transaction scope smaller FIXME?
+      # fobj.save() # Do we actually need to save it here, or can we
+      # make the transaction scope smaller FIXME?
       LOGGER.info("Creating archive record for %s." % fname)
     else:
       LOGGER.info("Copying %s to archive but not recording in repository." % fname)
 
   archpath = fobj.repository_file_path
 
-  # Copy file to archive. In case remote host is provided, scp via remote host. Otherwise copy.
+  # Copy file to archive. In case remote host is provided, scp via
+  # remote host. Otherwise copy.
   if force_overwrite or not os.path.exists(archpath):
     
     if archloc.host and archloc.host_port and archloc.host_path and archloc.host_user:
-      # TODO: fixme? The following line is a forced solution due to fobj.repository_file_path being able to deliver only repository primary location
-      # and mounted archive locations of the file; while here we need the path to the file in foreign host.
+      # TODO: fixme? The following line is a forced solution due to
+      # fobj.repository_file_path being able to deliver only
+      # repository primary location and mounted archive locations of
+      # the file; while here we need the path to the file in foreign
+      # host.
       host_archdir = os.path.join(archloc.host_path, fobj.libcode)
 
       LOGGER.info("Creating dir for the file in archive.")
-      retcode = create_foreign_dir(archloc.host, archloc.host_port, archloc.host_user, host_archdir)
+      retcode = create_foreign_dir(archloc.host, archloc.host_port,
+                                   archloc.host_user, host_archdir)
       if retcode != 0:
         raise ValueError("Error: Failed to create %s in archive."
                          % (host_archdir))  
       LOGGER.info("Copying %s to the archive." % (fname))
-      retcode = transfer_over_scp(repopath, '%s:%s' % (archloc.host, host_archdir), port=archloc.host_port, user=archloc.host_user)
+      retcode = transfer_over_scp(repopath, '%s:%s' % (archloc.host, host_archdir),
+                                  port=archloc.host_port, user=archloc.host_user)
       if retcode != 0:
         raise ValueError("Error: Failed to copy %s to archive (%s@%s:%s)." 
                          % (repopath, archloc.host_user, archloc.host, host_archdir))
@@ -192,7 +218,9 @@ def move_file_to_archive(fpath, archive, force_overwrite=False, force_delete=Fal
       if not os.path.exists(archdir):
         LOGGER.info("Creating dir for the file in archive.")
         os.makedirs(archdir)
-      # The advantage of shutil.copy2 over copy is that it tries to keep the file metadata. Equivalent to 'cp -p source destination'
+        # The advantage of shutil.copy2 over copy is that it tries to
+        # keep the file metadata. Equivalent to 'cp -p source
+        # destination'
         LOGGER.info("Copying file to the archive.")
       t0 = time.time()
       shutil.copy2(repopath, archpath)
@@ -221,10 +249,14 @@ def move_file_to_archive(fpath, archive, force_overwrite=False, force_delete=Fal
   return None
 
 def remove_primary_files(files, archive, filetype, force_delete=False):
-  '''Deletes primary copies of the archived files in case forced or archived more than specified number of days ago (archloc.host_delete_timelag).
-  In case list of files is empty, identifies all files that have been in archive more than specified number of days for a particular file type.'''
-
-  # in case emty list of files (i.e. consider deleting all 
+  '''
+  Deletes primary copies of the archived files in case forced or
+  archived more than specified number of days ago
+  (archloc.host_delete_timelag).  In case list of files is empty,
+  identifies all files that have been in archive more than specified
+  number of days for a particular file type.
+  '''
+  # in case empty list of files (i.e. consider deleting all 
   if len(files) == 0:
     archloc = ArchiveLocation.objects.get(name=archive)
     time_threshold = datetime.datetime.now() - datetime.timedelta(days=archloc.host_delete_timelag)
@@ -233,9 +265,11 @@ def remove_primary_files(files, archive, filetype, force_delete=False):
       files = Lanefile.objects.filter(filetype__code=filetype, archive_date__lt=time_threshold)
     if filetype == 'bam':
       files = Alnfile.objects.filter(filetype__code=filetype, archive_date__lt=time_threshold)
-    LOGGER.warning("Identified %d %s files archived more than %d days ago." % (len(files), filetype, archloc.host_delete_timelag))    
+    LOGGER.warning("Identified %d %s files archived more than %d days ago.",
+                   len(files), filetype, archloc.host_delete_timelag)
   if ARGS.force_delete:
-    LOGGER.warning("Attention: FORCED DELETION of primary copies for %d archived files!" % len(files))
+    LOGGER.warning("Attention: FORCED DELETION of primary copies for %d archived files!",
+                   len(files))
 
   # if file has been in Archive for long enough or force_delete, delete the source
   filesDeleted = 0
@@ -245,18 +279,24 @@ def remove_primary_files(files, archive, filetype, force_delete=False):
     fobj.archive = None
     repopath = fobj.repository_file_path
     if not force_delete:
-      LOGGER.info("More than %d days passed since archiving %s. (Archive date=%s\tToday=%s)." % (archloc.host_delete_timelag, repopath, fobj.archive_date, t_date))
+      LOGGER.info("More than %d days passed since archiving %s. (Archive date=%s\tToday=%s).",
+                  archloc.host_delete_timelag, repopath, fobj.archive_date, t_date)
       if os.path.exists(repopath):
         LOGGER.warning("Removing %s." % repopath)
     else:
-      LOGGER.warning("Executing forced deletion. Archive information: date=%s file=%s. Removing %s" % (fobj.archive_date, archpath, repopath))
+      LOGGER.warning("Executing forced deletion. Archive information: date=%s file=%s. Removing %s",
+                     fobj.archive_date, archpath, repopath)
 
-    # Before deleting the file, check that the file in archive not only exists but has the same md5 sum as recorded in repository
+    # Before deleting the file, check that the file in archive not
+    # only exists but has the same md5 sum as recorded in repository
     if os.path.exists(archpath):
       checksum = checksum_file(archpath)
       if checksum != fobj.checksum:
-        # raise ValueError("Error: Archive file checksum (%s) not same as in repository (%s)." % (checksum, fobj.checksum))
-        LOGGER.error("Error: Archive file checksum (%s) not same as in repository (%s). Can not delete the file!" % (checksum, fobj.checksum))
+        # raise ValueError("Error: Archive file checksum (%s) not same
+        # as in repository (%s)." % (checksum, fobj.checksum))
+        LOGGER.error(\
+          "Error: Archive file checksum (%s) not same as in repository (%s). Can not delete the file!",
+          checksum, fobj.checksum)
       else:
         if os.path.exists(repopath):
           os.unlink(repopath)
@@ -266,8 +306,9 @@ def remove_primary_files(files, archive, filetype, force_delete=False):
   if filesDeleted == 0:
     LOGGER.warning("No files to delete from repository.")
   else:
-    LOGGER.warning("%d files removed from repository." % filesDeleted)
+    LOGGER.warning("%d files removed from repository.", filesDeleted)
 
+################################################################################
 if __name__ == '__main__':
 
   # get list of Archives
@@ -283,8 +324,9 @@ if __name__ == '__main__':
 
   from argparse import ArgumentParser
 
-  PARSER = ArgumentParser(description='A script for safe copy of repository files to the default'
-                          + ' file archive and deletion of the originals (after a set time) in repository.')
+  PARSER = ArgumentParser(\
+    description='A script for safe copy of repository files to the default'
+    + ' file archive and deletion of the originals (after a set time) in repository.')
 
   PARSER.add_argument('files', metavar='<filenames>', type=str, nargs='*',
                       help='The name of the file or files to archive. The files'
@@ -295,19 +337,27 @@ if __name__ == '__main__':
                       help='Archive all files of a given file type.')
 
   PARSER.add_argument('-A', '--archive', dest='archive_name', default = archive,
-                      help='Name of the archive where the files should be copied. List of supported archives: %s' % ', '.join(arks))
+                      help='Name of the archive where the files should be copied.'
+                      + ' List of supported archives: %s' % ', '.join(arks))
 
   PARSER.add_argument('-c', '--copy', dest='copy_only', action='store_true',
                       help='Copy file(s) to archive without regisering them as archived.')
 
   PARSER.add_argument('-C', '--copy_wait_archive', dest='copy_wait_archive', action='store_true',
-                      help='Force archiving in following 3 stages: 1) copy files to the archive, 2) wait for 5 minutes for the file system to pick up the existance of the files, 3) checks md5sums and register files as  archived. The option was implemented to overcome a feature of CRI Archive which occasionally reports files missing even after 30s since ceration.')
+                      help='Force archiving in following 3 stages: 1) copy files'
+                      + ' to the archive, 2) wait for 5 minutes for the file system'
+                      + ' to pick up the existance of the files, 3) checks md5sums'
+                      + ' and register files as  archived. The option was implemented'
+                      + ' to overcome a feature of CRI Archive which occasionally'
+                      + ' reports files missing even after 30s since ceration.')
 
   PARSER.add_argument('-d', '--force_delete', dest='force_delete', action='store_true',
-                      help='Force source deletion even if less than N days have passed since archiving.')
+                      help='Force source deletion even if less than N days have'
+                      + ' passed since archiving.')
 
   PARSER.add_argument('-m', '--force_md5_check', dest='force_md5_check', action='store_true',
-                      help='Force md5 check on archived file. Adds archiving information in case it is missing.')
+                      help='Force md5 check on archived file. Adds archiving'
+                      + ' information in case it is missing.')
 
   PARSER.add_argument('-f', '--force_overwrite', dest='force_overwrite', action='store_true',
                       help='Force overwrite for files already archived.')
@@ -321,7 +371,8 @@ if __name__ == '__main__':
     raise ValueError("Error: Unknown archive \'%s\'. Exiting! " % (ARGS.archive_name))
   LOGGER.info("Archive is set to \'%s\'." % archive)
 
-  # In case file type was provided, override the list of files (if any) provided as arguments
+  # In case file type was provided, override the list of files (if
+  # any) provided as arguments
   if ARGS.filetype:
     fnames = get_files_for_filetype(ARGS.filetype, not_archived=True)
     LOGGER.warning("Found %d non-archived files for copying." % len(fnames))
@@ -331,7 +382,11 @@ if __name__ == '__main__':
     if ARGS.copy_wait_archive or ARGS.copy_only:
       for fname in fnames:
         LOGGER.warning("Copying \'%s\' to archive." % fname)
-        move_file_to_archive(str(fname), ARGS.archive_name, force_overwrite=ARGS.force_overwrite, force_delete=ARGS.force_delete, force_md5_check=ARGS.force_md5_check, copy_only=True)
+        move_file_to_archive(str(fname), ARGS.archive_name,
+                             force_overwrite=ARGS.force_overwrite,
+                             force_delete=ARGS.force_delete,
+                             force_md5_check=ARGS.force_md5_check,
+                             copy_only=True)
 
     # Wait for files copied to archive to become visible in the file system
     if ARGS.copy_wait_archive:
@@ -344,16 +399,20 @@ if __name__ == '__main__':
       LOGGER.warning("Archiving %d non-archived files:" % len(fnames))
       for fname in fnames:
         LOGGER.warning("Archiving \'%s\'." % fname)
-        failedfn = move_file_to_archive(str(fname), ARGS.archive_name, force_overwrite=ARGS.force_overwrite, force_delete=ARGS.force_delete, force_md5_check=ARGS.force_md5_check)
+        failedfn = move_file_to_archive(str(fname), ARGS.archive_name,
+                                        force_overwrite=ARGS.force_overwrite,
+                                        force_delete=ARGS.force_delete,
+                                        force_md5_check=ARGS.force_md5_check)
         if failedfn is not None:
           failedfns.append(failedfn)
     if failedfns:
-      LOGGER.error("%d failed archiving due to md5 check sum differences:" % len(failedfns))
+      LOGGER.error("%d failed archiving due to md5 check sum differences:", len(failedfns))
       for failedfn in failedfns:
-        LOGGER.error("%s" % failedfn)        
+        LOGGER.error("%s" % failedfn)
 
   # Check for deletion and delete primary copies of files archived long time ago.
   if ARGS.filetype:
     fnames = []
   if ARGS.force_delete or len(fnames) == 0:
-    remove_primary_files(fnames, ARGS.archive_name, ARGS.filetype, force_delete=ARGS.force_delete)
+    remove_primary_files(fnames, ARGS.archive_name, ARGS.filetype,
+                         force_delete=ARGS.force_delete)
