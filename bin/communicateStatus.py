bd74f48ba51fa21563574b583ec557e16af08eab
Margus Lukk <margus.lukk@cruk.cam.ac.uk>
2016-06-08 10:41:10 +0100

    Added feature of communicating lane status using separate script either directly to repository or over ssh to host which has access to the repository.

--- bin/communicateStatus.py
+++ bin/communicateStatus.py
@@ -2,22 +2,33 @@
 #
 # $Id$
 
-import sys
-import os
-from communicate import CommunicateStatus
-
 if __name__ == '__main__':
     import argparse
-
+    
     PARSER = argparse.ArgumentParser(
-        description='communicate Lane status to repository.')
-
-    PARSER.add_argument('--lane_id', dest='lane_id', type=int, help='Lane ID', required = True)
+        description='communicate Lane status to repository. Creates new lane in case lane with flowcell & flowlane & library & status is missing.')
+    
     PARSER.add_argument('--status', dest='status', type=str, help='Lane status. E.g. \'qc in progress\' or \'run complete\'', required = True)
+    PARSER.add_argument('--flowlane', dest='flowlane', type=str, help='flowlane of the lane', required = False)
+    PARSER.add_argument('--library', dest='library', type=str, help='library (i.e. donumber)', required = False)
+    PARSER.add_argument('--facility', dest='facility', type=str, help='Facility code (e.g. CRI)', required = False)
+    GROUP = PARSER.add_mutually_exclusive_group()
+    GROUP.add_argument('--laneid', dest='laneid', type=int, help='Lane ID')
+    GROUP.add_argument('--flowcell', dest='flowcell', type=str, help='Flowcell of the lane')
+    GROUP.add_argument('--filename', dest='fname', type=str, help='Filename')
 
     ARGS = PARSER.parse_args()
 
-#    lane_id = 82957 #(status_id=62172) status = 'complete' # (status_id=62175)
-#    status = 'qc in progress' # (status_id=62175)
+    #   lane_id = 82957 #(status_id=62172) status = 'complete' # (status_id=62175)
+    #   status = 'qc in progress' # (status_id=62175)
     cm = CommunicateStatus()
-    cm.setLaneStatus(ARGS.lane_id, ARGS.status)
+
+    if ARGS.laneid is not None:
+        cm.setLaneStatus(ARGS.laneid, ARGS.status)
+    elif ARGS.flowcell:
+        if ARGS.flowlane is not None and ARGS.library is not None and ARGS.facility is not None:
+            cm.setLaneStatusByFlowcell(ARGS.library, ARGS.flowcell, ARGS.flowlane, ARGS.facility, ARGS.status)
+        else:
+            raise SystemExit("To set lane status by flowcell, flowlane, facility and library need to be specified.")
+    elif ARGS.fname is not None:
+        cm.setLaneStatusByFilename(ARGS.fname, ARGS.status)
