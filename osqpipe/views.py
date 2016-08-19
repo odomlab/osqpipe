1e794dc8c7854e24c753ccce4356dd8267435edc
Tim Rayner <tim.rayner@cruk.cam.ac.uk>
2016-08-19 14:47:37 +0100

    Adding web UI support for a basic sample page to expose sample characteristic metadata for our users.

--- osqpipe/views.py
+++ osqpipe/views.py
@@ -14,7 +14,7 @@ from django.views.generic.edit import FormMixin
 from collections import OrderedDict
 
 from .models import Library, Project, Genome, Lane, Alnfile, Lanefile, QCfile,\
-    Peakfile, MergedAlignment, MergedAlnfile, HistologyImagefile, Alignment
+    Peakfile, MergedAlignment, MergedAlnfile, HistologyImagefile, Alignment, Sample
 from .forms import SimpleSearchForm, LibrarySearchForm, LibraryEditForm,\
     LibraryProjectPicker
 
@@ -385,6 +385,28 @@ class LaneDetailView(MyDetailView):
     self.request.session['session_lane'] = object.pk
     self.request.session['session_library'] = object.library.code
     return super(LaneDetailView, self).get(request, *args, **kwargs)
+
+class SampleDetailView(MyDetailView):
+  context_object_name = 'sample'
+  template_name       = 'repository/sample/detail.html'
+  model               = Sample
+
+  # Dummy value for pylint.
+  kwargs              = {}
+
+  def get(self, request, *args, **kwargs):
+    object = get_object_or_404(self.model, id=self.kwargs['pk'])
+
+    # Per-project user authorization.
+    allowed_users = [ person for project in object.library.projects.all()
+                             for person  in project.people.all() ]
+    if self.request.user not in allowed_users:
+      return redirect('denied')
+
+    # FIXME ideally we also need to get session_library set here somehow. Also
+    # fix the sitetree stuff.
+    self.request.session['session_sample'] = object.pk
+    return super(SampleDetailView, self).get(request, *args, **kwargs)
 
 class QualplotDetailView(LaneDetailView):
   template_name       = 'repository/lane/qualplot.html'
