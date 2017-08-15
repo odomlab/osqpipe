a7ef5044b812833453f23e2134d720d6558e5477
Admin <admin@dolab-srv003>
2017-08-15 10:55:49 +0100

    Adding the ability to download Lane tarballs from the library list page.

--- osqpipe/views.py
+++ osqpipe/views.py
@@ -137,7 +137,8 @@ class LibraryListView(FilterMixin, FormListView):
   def get_context_data(self, *args, **kwargs):
     '''Add extra context to control the view.'''
     context = super(LibraryListView, self).get_context_data(*args, **kwargs)
-    context['linked_filetypes'] = ('fasta', 'fastq', 'bam')
+    context['lane_linked_filetypes'] = ('fasta', 'fastq', 'tar')
+    context['aln_linked_filetypes'] = ('bam',)
     context['project_code']     = self.kwargs['project']
     return context
 
