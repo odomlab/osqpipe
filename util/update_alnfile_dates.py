b271feab710d76ef701279c2e6ddc750807317c8
Tim Rayner <tim.rayner@cruk.cam.ac.uk>
2018-04-25 12:52:28 +0100

    Adding license notices to all code files, as recommended by the GNU GPL howto: https://www.gnu.org/licenses/gpl-howto.html

--- util/update_alnfile_dates.py
+++ util/update_alnfile_dates.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+#
+# Copyright 2018 Odom Lab, CRUK-CI, University of Cambridge
+#
+# This file is part of the osqpipe python package.
+#
+# The osqpipe python package is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# The osqpipe python package is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the osqpipe python package.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 '''Very simple script to find files in the directories under the
 current working directory, search the repository for the corresponding
